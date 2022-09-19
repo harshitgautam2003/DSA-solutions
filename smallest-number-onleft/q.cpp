6a0fc641d88d65e65700e72c467b577af7631637
Harshit GAutam <harshitgtm02@gmail.com>
2022-09-19 23:46:07 +0530

    07

---
+++ smallest-number-onleft/q.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
+        
+        stack<int> st;
+        st.push(-1);
+        
+        vector<int> ans;
+        
+        for(int i=0; i<n; i++)
+        {
+            while(st.top() >= a[i])
+                st.pop();
+            
+            ans.push_back(st.top());
+            
+            if(i != n && a[i] < a[i+1])
+                st.push(a[i]);
+        }
+        
+        return ans;
+    }
+};
