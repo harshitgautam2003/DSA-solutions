ed5e8c575ba45a68c2870a7aa377225785f372ce
Harshit GAutam <harshitgtm02@gmail.com>
2022-09-19 23:38:42 +0530

    06

---
+++ google-kick-rE-q1/q1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    for(int i=1;i<=t;i++){
+        int n,a;cin>>n;
+        a=n/5+1;
+        if(n%5==0) a-=1;
+        cout<<"Case #"<<i<<": "<<a<<endl;
+    }
+}
