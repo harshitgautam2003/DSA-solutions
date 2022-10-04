50d2161b0acd6432c0a9681012d86d07f1a5ef14
Harshit GAutam <harshitgtm02@gmail.com>
2022-10-04 18:36:44 +0530

    22

---
+++ arr_to_zig-zag/zig-zag.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:	
+	// Program for zig-zag conversion of array
+	void zigZag(int arr[], int n) {
+	    
+	    // code here
+	     for(int i=0;i<n-1;i++){
+            if(i%2==0 && arr[i]>arr[i+1]){
+                swap(arr[i],arr[i+1]);
+            }
+            else if(i%2==1 && arr[i]<arr[i+1]){
+                swap(arr[i],arr[i+1]);
+            }
+	    }
+	}
+};
