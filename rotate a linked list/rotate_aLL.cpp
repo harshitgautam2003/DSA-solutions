1e0d9af29ca7623f3a279db7c74e951cfb07ae39
Harshit GAutam <harshitgtm02@gmail.com>
2022-10-26 21:57:59 +0530

    43

---
+++ rotate a linked list/rotate_aLL.cpp
@@ -0,0 +1,35 @@
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+        Node*p=head;Node*q=head;
+        while(p->next!=NULL){
+            p=p->next;
+        }
+        while(k>1){
+            q=q->next;
+            k--;
+        }
+        p->next=head;
+        head=q->next;
+        q->next=NULL;
+        return head;
+    }
+};
+    
