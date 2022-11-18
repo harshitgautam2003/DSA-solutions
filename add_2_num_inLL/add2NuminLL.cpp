7614b5df0ff544d32be50096699eb2811670fc5e
Harshit GAutam <harshitgtm02@gmail.com>
2022-11-18 19:01:39 +0530

    73

---
+++ add_2_num_inLL/add2NuminLL.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void add(ListNode**p,int data){
+        ListNode *a=new ListNode(data);
+        if(*p==NULL){*p=a;return;}
+        else{
+            ListNode*q=*p;while(q->next!=NULL){
+                q=q->next;
+            }
+            q->next=a;
+        }
+        return;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+       ListNode *ans = NULL;
+        int carry = 0;
+        int sum = l1->val + l2->val;
+        while (l1 || l2 || carry)
+        {
+            int val1 = 0, val2 = 0;
+            if (l1)
+                val1 = l1->val;
+            if (l2)
+                val2 = l2->val;
+            sum = val1 + val2 + carry;
+
+            add(&ans, sum % 10);
+            carry = sum / 10;
+
+            if (l1)
+                l1 = l1->next;
+            if (l2)
+                l2 = l2->next;
+        }
+        return ans;
+
+    }
+};
