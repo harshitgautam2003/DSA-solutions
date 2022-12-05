f2a81cc0db4fe29ec990cff5e1926a122bbc64a2
Harshit GAutam <harshitgtm02@gmail.com>
2022-12-05 18:21:25 +0530

    95

---
+++ LC_Q876/middle_ofLL.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode*p=head,*q=head;
+        while(p->next!=NULL){
+            q=q->next;
+            p=p->next;
+            if(p->next)p=p->next;
+        }
+        return q;
+    }
+};
