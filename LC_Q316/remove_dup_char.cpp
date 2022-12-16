819175616a30ac35bd42d8376969a9ba96dc0c4f
Harshit GAutam <harshitgtm02@gmail.com>
2022-12-16 16:53:49 +0530

    115

---
+++ LC_Q316/remove_dup_char.cpp
@@ -0,0 +1,16 @@
+vector <int> arr(26,0),chk(26,0);int n=s.size(); string ans="";
+        for(int i=0;i<n;i++){
+            arr[s[i]-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            arr[s[i]-'a']--;
+            if(!chk[s[i]-'a']){
+                while(ans.length()>0 && ans.back()>s[i] && arr[ans.back()-'a']>0){
+                    chk[ans.back()-'a']=0;
+                    ans.pop_back();
+                }
+                ans+=s[i];
+                chk[s[i]-'a']=1;
+            }
+        }
+        return ans;
