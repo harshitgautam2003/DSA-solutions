6acb140de3bb5582ee39a932a40a6befba807792
Harshit GAutam <harshitgtm02@gmail.com>
2023-01-15 13:12:52 +0530

    c

---
+++ LEETCODE/LC_Q9/palindrome_num.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if( x < 0 ) return false ;
+        int n = x ;
+         // int rev = 0 ;
+        long long int rev = 0 ;
+        while( n != 0 ) {
+            int digit = n%10 ;
+            // if ( rev > INT_MAX/10 ) return false ;
+            rev = rev*10 + digit ;
+            n /= 10 ;
+        }
+       return x==rev ;
+    }
+};
